6c97225a233e4bbb3c08b6496dd8ac1fd8fd13fc
Dung <dungvuapokiwar2004@gmail.com>
2026-07-14 21:27:27 +0700

    Dlua Music

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'dlua-music.vercel.app' }));
 app.use(express.json());
 app.use('/api/songs', songRoutes);
 app.use('/api/auth', authRoutes);
