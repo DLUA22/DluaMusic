4076d684a62bbfef7e2ddf8a284c3efdb57e28fe
Dung <dungvuapokiwar2004@gmail.com>
2026-07-14 21:24:28 +0700

    Dlua Music

--- frontend-web/src/services/api.js
+++ frontend-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://dluamusic.onrender.com', 
+  baseURL: 'https://dluamusic.onrender.com/api', 
   timeout: 10000,
 });
 
